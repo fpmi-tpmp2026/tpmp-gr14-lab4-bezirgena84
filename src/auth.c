e46c03d2a96b8b4ad0cd406e3a206057a9211a84
bezirgena84 <bezirgena84@gmail.com>
2026-04-01 22:11:32 +0300

    Update auth.c

--- src/auth.c
+++ src/auth.c
@@ -1,65 +1,67 @@
-#include "auth.h"
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <openssl/sha.h>
+#include "../includes/auth.h"
+#include "../includes/database.h"
 
-void auth_hash_password(const char *password, char *hash) {
-    unsigned char digest[SHA256_DIGEST_LENGTH];
-    SHA256((unsigned char*)password, strlen(password), digest);
+static User current_user = {"", "", 0};
+static bool authenticated = false;
+
+void hash_password(const char* password, char* output) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, password, strlen(password));
+    SHA256_Final(hash, &sha256);
     
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        sprintf(&hash[i*2], "%02x", digest[i]);
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        sprintf(output + (i * 2), "%02x", hash[i]);
     }
-    hash[SHA256_DIGEST_LENGTH * 2] = '\0';
+    output[64] = '\0';
 }
 
-bool auth_login(sqlite3 *db, const char *username, const char *password, User *user) {
-    char hash[65];
-    auth_hash_password(password, hash);
+bool authenticate(const char* username, const char* password) {
+    char sql[512];
+    char hashed_pass[65];
+    hash_password(password, hashed_pass);
     
-    const char *sql = "SELECT id, username, password_hash FROM user WHERE username = ? AND password_hash = ?;";
-    sqlite3_stmt *stmt;
+    sprintf(sql, "SELECT username, is_admin FROM users WHERE username='%s' AND password='%s'", 
+            username, hashed_pass);
     
-    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
+    sqlite3_stmt* stmt;
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+    
+    if (rc != SQLITE_OK) {
         return false;
     }
     
-    sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 2, hash, -1, SQLITE_STATIC);
-    
-    bool success = false;
     if (sqlite3_step(stmt) == SQLITE_ROW) {
-        user->id = sqlite3_column_int(stmt, 0);
-        strncpy(user->username, (const char*)sqlite3_column_text(stmt, 1), 50);
-        strncpy(user->password_hash, (const char*)sqlite3_column_text(stmt, 2), 128);
-        success = true;
+        strcpy(current_user.username, (const char*)sqlite3_column_text(stmt, 0));
+        current_user.is_admin = sqlite3_column_int(stmt, 1);
+        authenticated = true;
+        sqlite3_finalize(stmt);
+        return true;
     }
     
     sqlite3_finalize(stmt);
-    return success;
+    return false;
 }
 
-bool auth_register(sqlite3 *db, const char *username, const char *password) {
-    char hash[65];
-    auth_hash_password(password, hash);
-    
-    const char *sql = "INSERT INTO user (username, password_hash) VALUES (?, ?);";
-    sqlite3_stmt *stmt;
-    
-    if (sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) != SQLITE_OK) {
-        return false;
-    }
-    
-    sqlite3_bind_text(stmt, 1, username, -1, SQLITE_STATIC);
-    sqlite3_bind_text(stmt, 2, hash, -1, SQLITE_STATIC);
-    
-    int rc = sqlite3_step(stmt);
-    sqlite3_finalize(stmt);
-    
-    return rc == SQLITE_DONE;
+bool is_authenticated(void) {
+    return authenticated;
 }
 
-void auth_clear_user(User *user) {
-    if (user) {
-        memset(user, 0, sizeof(User));
-    }
+void logout(void) {
+    authenticated = false;
+    memset(&current_user, 0, sizeof(User));
+}
+
+void set_current_user(const char* username) {
+    strcpy(current_user.username, username);
+}
+
+const char* get_current_user(void) {
+    return current_user.username;
 }
